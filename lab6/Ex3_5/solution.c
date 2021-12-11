1d405e17e68b675845dced41bceff235411383bb
Vladosik02 <XA12850420@student.karazin.ua>
2021-12-11 15:44:07 +0200

    Add files solution.c and makefile to folder Ex3_5

---
+++ lab6/Ex3_5/solution.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+	int pid = fork();
+	if (pid == 0)
+	{
+		setsid();
+		chdir("/");
+		printf("%d\n", getppid());
+		fclose(stdin);
+		fclose(stdout);
+		fclose(stderr);
+		sleep(10000);
+		exit(0);
+	} else if (pid == -1)
+	{
+		printf("Error\n");
+	} 
+	return 0;
+}
